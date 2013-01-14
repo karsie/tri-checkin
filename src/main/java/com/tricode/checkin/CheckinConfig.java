cad6d14034aa48961db1ee7940cefde7f3658e52
Kars Blom <karsie@zonnet.nl>
2013-01-14 17:32:04 +0100

    - added initial xml loader task - added configuration class

---
+++ src/main/java/com/tricode/checkin/CheckinConfig.java
@@ -0,0 +1,25 @@
+package com.tricode.checkin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ */
+@Configuration
+@PropertySource("classpath:/checkin.properties")
+@EnableScheduling
+@EnableWebMvc
+public class CheckinConfig {
+
+    @Autowired
+    private Environment environment;
+
+    public String getXmlFile() {
+        return environment.getProperty("checkin.xml.file");
+    }
+
+}
---
+++ src/main/java/com/tricode/checkin/task/PersonXmlLoaderTask.java
@@ -0,0 +1,41 @@
+package com.tricode.checkin.task;
+
+import com.tricode.checkin.CheckinConfig;
+import com.tricode.checkin.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ */
+@Component
+public class PersonXmlLoaderTask {
+
+    private final CheckinConfig checkinConfig;
+    private final PersonService personService;
+
+    @Autowired
+    public PersonXmlLoaderTask(CheckinConfig checkinConfig, PersonService personService) {
+        this.checkinConfig = checkinConfig;
+        this.personService = personService;
+    }
+
+//    @Scheduled(cron = "0 3 * * * ?")
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void load() {
+        try {
+            checkinConfig.getXmlFile();
+        } catch (Exception e) {
+            reSchedule();
+        }
+    }
+
+    private void reSchedule() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                load();
+            }
+        });
+    }
+}
