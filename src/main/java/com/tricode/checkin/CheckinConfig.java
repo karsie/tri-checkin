850b8ba3c6a99e1aa0f9f0947cb4446af24985f8
Kars Blom <kars.arnout.blom@gmail.com>
2013-01-16 20:40:56 +0100

    - simplified properties loading - added comment sections to dependencies

--- src/main/java/com/tricode/checkin/CheckinConfig.java
+++ src/main/java/com/tricode/checkin/CheckinConfig.java
@@ -1,9 +1,8 @@
 package com.tricode.checkin;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,11 +12,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class CheckinConfig {
 
-    @Autowired
-    private Environment environment;
+    @Value("${checkin.xml.file}")
+    private String xmlFile;
 
     public String getXmlFile() {
-        return environment.getProperty("checkin.xml.file");
+        return xmlFile;
     }
 
 }
