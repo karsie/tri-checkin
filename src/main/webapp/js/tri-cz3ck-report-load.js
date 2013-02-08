7ce39d624307be5de0dd8fb12095af9ac51b53ee
jlagerweij <jos@lagerweij>
2013-02-08 18:53:24 +0100

    Add ability to run Jetty from Gradle.
    - Removed ${minifySuffix} from files.
    - Added jetty Gradle plugin.
    - Removed minifying from build.gradle.

--- src/main/webapp/js/tri-cz3ck-report-load.js
+++ src/main/webapp/js/tri-cz3ck-report-load.js
@@ -4,11 +4,11 @@ requirejs.config({
             deps: ['libs/underscore-min', 'libs/jquery-min'],
             exports: 'Backbone'
         },
-        'libs/backbone.subset${minifySuffix}': {
+        'libs/backbone.subset': {
             deps: ['libs/backbone-min']
         },
         'libs/jquery-ui-min': {
-            deps: ['libs/jquery-min', 'libs/require-css${minifySuffix}'],
+            deps: ['libs/jquery-min', 'libs/require-css'],
             init: function () {
                 requireCss("libs/jquery-ui-min");
             }
@@ -16,19 +16,19 @@ requirejs.config({
         'libs/jcanvas-min': {
             deps: ['libs/jquery-min']
         },
-        'tri-cz3ck-models${minifySuffix}': {
-            deps: ['libs/backbone-min', 'libs/backbone.subset${minifySuffix}']
+        'tri-cz3ck-models': {
+            deps: ['libs/backbone-min', 'libs/backbone.subset']
         },
-        'tri-cz3ck-report-views${minifySuffix}': {
-            deps: ['tri-cz3ck-models${minifySuffix}', 'libs/jquery-ui-min', 'libs/jcanvas-min', 'libs/require-css${minifySuffix}'],
+        'tri-cz3ck-report-views': {
+            deps: ['tri-cz3ck-models', 'libs/jquery-ui-min', 'libs/jcanvas-min', 'libs/require-css'],
             init: function() {
-                requireCss("tri-cz3ck${minifySuffix}");
+                requireCss("tri-cz3ck");
             }
         }
     }
 });
 
-define(['tri-cz3ck-report-views${minifySuffix}'], function () {
+define(['tri-cz3ck-report-views'], function () {
     var reportList = new WeekReportDataList;
     var employees = new Employees;
     $(function() {
